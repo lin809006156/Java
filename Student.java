d9df84874f52d67a025d944fda8909d0aeed7c0b
lin809006156 <115612717+lin809006156@users.noreply.github.com>
2024-11-07 13:57:52 +0000

    Delete Student.java

--- Student.java
+++
@@ -1,8 +0,0 @@
-package staticTutor2;
-
-public class Student {
-    public static void printHelloWorld() {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-    }
-}
