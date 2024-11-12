9120dc888340e1ca6ff7e52f75a752faca7d1d97
lin809006156 <115612717+lin809006156@users.noreply.github.com>
2024-11-12 20:01:09 +0000

    Add files via upload

---
+++ StudentOperator.java
@@ -0,0 +1,8 @@
+package interfacePractice;
+
+import java.util.LinkedList;
+
+public interface StudentOperator {
+    void printAllInfo(LinkedList<Student> students);
+    void printAverageScore(LinkedList<Student> students);
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+package interfacePractice;
+
+public class Test {
+    public static void main(String[] args) {
+        ClassManager classManager = new ClassManager();
+        classManager.printInfo();
+        classManager.printScore();
+    }
+}
